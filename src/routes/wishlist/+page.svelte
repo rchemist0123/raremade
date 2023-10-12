371d7f4c29789b05aafe403ef040f87fae5bcdd6
rchemist0123 <aidenjang94@gmail.com>
2023-10-12 09:44:04 +0900

    ui

--- src/routes/wishlist/+page.svelte
+++ src/routes/wishlist/+page.svelte
@@ -1,14 +1,16 @@
 <script>
   export let data;
 </script>
-<div class="flex flex-col items-center">
-  <h1 class="text-3xl">Wishlist</h1>
-  <div class="flex flex-col gap-4">
+<div class="w-full flex flex-col items-center gap-y-10 py-20">
+  <h1 class="text-6xl font-bold">Wishlist</h1>
+  <div class="w-full flex flex-col items-center">
     {#each data.items as item}
-    <div class="flex flex-row">
-      <img class="w-20 h-20" src={item.photo_dir} alt=""/>
+    <div class="w-full flex flex-row p-16 gap-10 justify-between items-center">
+      <img class="w-36 h-36" src={item.photo_dir} alt=""/>
       <p>{item.slug}</p>
-      <p>{item.price}</p>
+      <p class="text-gray-500">₩{item.price}</p>
+      <p>In stock</p>
+      <button class="bg-pink-500 px-10 py-5 text-white">ADD TO CART</button>
     </div>
     {/each}
   </div>
