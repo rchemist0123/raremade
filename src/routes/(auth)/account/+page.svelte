709c463e570b576a06ba85ca6d450754cf44330f
rchemist0123 <aidenjang94@gmail.com>
2023-10-12 17:10:58 +0900

    user

---
+++ src/routes/(auth)/account/+page.svelte
@@ -0,0 +1,37 @@
+<script>
+  
+</script>
+
+<div class="p-10">
+  <!-- Login title -->
+  <h1 class="text-3xl py-5 mx-auto">
+    Login
+  </h1>
+  <!-- Login form -->
+  <form class="border border-slate-400 px-5 py-5 space-y-5 rounded-lg flex flex-col">
+    <!-- ID or email -->
+    <div class="space-y-1">
+      <label for="">
+        <p class="text-slate-600 font-semibold">EMAIL</p>
+      </label>
+      <input class="border border-gray-300 w-full" type="email" >
+    </div>
+    <!-- Password -->
+    <div class="space-y-1">
+      <label for="">
+        <p class="text-slate-600 font-semibold">PASSWORD</p>
+      </label>
+      <input class="border border-gray-300 w-full" type="password" >
+    </div>
+    <!-- Login button -->
+    <div class="">
+      <button class="bg-pink-500 px-3 py-3 text-white text-xl w-full md:w-1/2">로그인</button>
+    </div>
+    <!-- Signup -->
+    <div class="text-center md:text-left">
+      <a class="text-pink-500" href="/signup">회원이 아니신가요?</a>
+    </div>
+    <!-- Login APIs -->                      
+    <!-- Kakao, Daum, Google, Apple ... -->
+  </form>
+</div>
---
+++ src/routes/(auth)/signup/+page.svelte
@@ -0,0 +1,48 @@
+<script>
+// @ts-nocheck
+</script>
+
+<div class="p-10">
+  <!-- Login title -->
+  <h1 class="text-3xl py-5 mx-auto">
+    Sign up
+  </h1>
+  <!-- Login box -->
+  <form class="border border-slate-400 px-5 py-5 space-y-5 rounded-lg flex flex-col"
+    method="POST" action="?/create"
+  >
+    <!-- ID or email -->
+    <div class="space-y-1">
+      <label for="">
+        <p class="text-slate-600 font-semibold">EMAIL</p>
+        <input class="border border-gray-300 w-full" type="text" name="email">
+      </label>
+    </div>
+    <!-- Name -->
+    <div class="space-y-1">
+      <label for="">
+        <p class="text-slate-600 font-semibold">NAME</p>
+        <input class="border border-gray-300 w-full" type="text" name="name">
+      </label>
+    </div>
+    <!-- Phone number -->
+    <div class="space-y-1">
+      <label for="">
+        <p class="text-slate-600 font-semibold">PHONE</p>
+      </label>
+      <input class="border border-gray-300 w-full" type="number" name="phone" >
+    </div>
+    <!-- Password -->
+    <div class="space-y-1">
+      <label for="">
+        <p class="text-slate-600 font-semibold">PASSWORD</p>
+      </label>
+      <input class="border border-gray-300 w-full" type="password" name="password">
+    </div>
+    <!-- Signup button -->
+    <div class="">
+      <button class="bg-pink-500 px-3 py-3 text-white text-xl w-full md:w-1/2">회원가입</button>
+    </div>                 
+    <!-- TODO: redirect to Sign up -->
+  </form>
+</div>
